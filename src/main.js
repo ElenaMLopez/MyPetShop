f88abe40a24a6b14964d96dae7bfd59e4210dc2a
Elena Mateos López <elena.mateos1@gmail.com>
2020-06-01 15:15:49 +0000

    Add Vuex to manage favorites dogs list

--- src/main.js
+++ src/main.js
@@ -4,23 +4,21 @@ import vuetify from "@/plugins/vuetify";
 import VueRouter from "vue-router";
 import Home from "./views/Home";
 import Pets from "./views/Pets";
+import Favorites from "./views/Favorites";
+import store from "./store/store";
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
 const routes = [
-  {
-    path: "/",
-    component: Home
-  },
-  {
-    path: "/pets",
-    component: Pets
-  }
+  { path: "/", component: Home },
+  { path: "/pets", component: Pets },
+  { path: "/favorites", component: Favorites }
 ];
 const router = new VueRouter({ routes });
 
 new Vue({
   vuetify,
   router,
+  store,
   render: h => h(App)
 }).$mount("#app");
--- src/views/Pets.vue
+++ src/views/Pets.vue
@@ -2,7 +2,7 @@
   <v-container grid-list-md fluid>
     <v-layout wrap>
       <v-flex xs12 sm4 md3 v-for="pet in dogs" :key="pet.breed">
-        <app-dog :dog="pet"></app-dog>
+        <app-dog :dog="pet" @addToFavorites="addToFavorites"></app-dog>
       </v-flex>
     </v-layout>
   </v-container>
@@ -12,6 +12,7 @@
 import { Dogs } from "../data/dogs";
 import Dog from "../components/Dog.vue";
 import axios from "axios";
+import { mapActions } from "vuex";
 
 axios.defaults.baseURL = "https://dog.ceo/api";
 
@@ -38,6 +39,9 @@ export default {
         });
       })
     );
+  },
+  methods: {
+    ...mapActions(["addToFavorites"])
   }
 };
 </script>
